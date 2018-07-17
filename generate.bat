5f5e2067148f430e457862c9df858d06ab19c0b9
OffColour <gary.walvin@oakbrook.local>
2018-07-17 09:12:27 +0100

    XSLT remaned

--- generate.bat
+++ generate.bat
@@ -2,7 +2,7 @@
 REM https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.3.1-windows-x86_64.zip
 
 
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat.exe" dir "Z:\WIX\Filebeat\source" -cg FilebeatFiles -gg -scom -sreg -sfrag -srd -dr INSTALLLOCATION  -var "var.FilebeatFilesDir" -t "excludeExe.xslt" -out "FilesFragment.wxs"
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat.exe" dir "Z:\WIX\Filebeat\source" -cg FilebeatFiles -gg -scom -sreg -sfrag -srd -dr INSTALLLOCATION  -var "var.FilebeatFilesDir" -t "FragmentTransform.xslt" -out "FilesFragment.wxs"
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" "filebeat.wxs" -arch x64
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" "FilesFragment.wxs" -arch x64
 "C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -v filebeat.wixobj FilesFragment.wixobj -o filebeat.msi
